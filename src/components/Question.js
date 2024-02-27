64076e05570c2b6237447d783a5a8de47ccd678b
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2024-02-27 17:11:30 -0500

    completed

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from 'react';
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    console.log(timeRemaining);
+
+    let timer = setTimeout(() => {
+      if (timeRemaining > 1) {
+        setTimeRemaining(timeRemaining - 1);
+        console.log('timer: ' + timer);
+      } else {
+        setTimeRemaining(10);
+        onAnswered(false);
+      }
+    }, 1000);
+
+    return () => {
+      clearTimeout(timer);
+      console.log('timer ' + timer + ' is cleared!');
+    };
+  });
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
